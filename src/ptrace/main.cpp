b3b32c6bb7eb453db182120cd63a37ba4e49a6e0
Veiasai <o206@qq.com>
2019-10-22 05:23:37 +0000

    monitor file openning successfully

--- src/ptrace/main.cpp
+++ src/ptrace/main.cpp
@@ -5,10 +5,31 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+#include <iostream>
 #include <stdio.h>
+#include <fcntl.h>
 
-int main(){
+using namespace std;
+
+int getFileName(pid_t child, unsigned long long addr, char * buffer, int len){
+    for(int i=0;i<len;i++){
+        long val = ptrace(PTRACE_PEEKDATA,child,addr+i*sizeof(long),NULL);
+        char * c = (char *)&val;
+        for (int j=0;j<8;j++){
+            buffer[i*sizeof(long)+j] = c[j];
+            if (c[j] == '\0'){
+                return 0;
+            }
+        }
+    }
+    return -1;
+}
+int main(int argc,char **argv){
+
+    if (argc != 2){
+        printf("please input the target file name\n");
+        return -1;
+    }
     pid_t child;
     long orig_rax;
     int status;
@@ -17,21 +38,34 @@ int main(){
 
     child = fork();
     if(child==0){
+        int childLogFd = open("childLog", O_CREAT | O_RDWR | O_APPEND, 0x666);
+        if (childLogFd < 0){
+            printf("fail to open childlog\n");
+            return -1;
+        }
+        dup2(childLogFd, 1);
+        dup2(childLogFd, 2);
         ptrace(PTRACE_TRACEME,0,NULL,NULL);
-        execl("/bin/ls","ls","-l","-h",NULL);
+        execvp(argv[1], NULL);
     } else {
         while(1){
             wait(&status);
             if(WIFEXITED(status))
                 break;
             orig_rax=ptrace(PTRACE_PEEKUSER,child,8*ORIG_RAX,NULL);
-            if(orig_rax == SYS_write){
+
+            if(orig_rax == SYS_open){
                 ptrace(PTRACE_GETREGS,child,NULL,&regs);
                 if(!iscalling){
                     iscalling =1;
-                    printf("SYS_write call with %lld, %lld, %lld\n",regs.rdi,regs.rsi,regs.rdx);
+                    char s[100];
+                    if (getFileName(child, regs.rdi, s, 100) < 0){
+                        printf("error\n");
+                        continue;
+                    };
+                    printf("SYS_open call with %lld, %lld, %lld, %s\n",regs.rdi,regs.rsi,regs.rdx, s);
                 } else{
-                    printf("SYS_write call return %lld\n",regs.rax);
+                    printf("SYS_open call return %lld\n",regs.rax);
                     iscalling = 0;
                 }                                  
             }
