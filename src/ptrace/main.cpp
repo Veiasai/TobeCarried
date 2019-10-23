8b222f58e85abb99aeeb9738e0964b787f7191f0
Tao Bocheng <Gusabary@126.com>
2019-10-23 16:45:03 +0800

    feat: can print every syscall invoked by all threads

--- src/ptrace/main.cpp
+++ src/ptrace/main.cpp
@@ -8,36 +8,26 @@
 #include <iostream>
 #include <stdio.h>
 #include <fcntl.h>
+#include <set>
+#include <map>
 
 using namespace std;
 
-int getFileName(pid_t child, unsigned long long addr, char * buffer, int len){
-    for(int i=0;i<len;i++){
-        long val = ptrace(PTRACE_PEEKDATA,child,addr+i*sizeof(long),NULL);
-        char * c = (char *)&val;
-        for (int j=0;j<8;j++){
-            buffer[i*sizeof(long)+j] = c[j];
-            if (c[j] == '\0'){
-                return 0;
-            }
-        }
-    }
-    return -1;
-}
 int main(int argc,char **argv){
-
     if (argc != 2){
         printf("please input the target file name\n");
         return -1;
     }
+
     pid_t child;
     long orig_rax;
     int status;
-    int iscalling=0;
     struct user_regs_struct regs;
+    map<int, set<int>> syscall;  // map of <tid, set<syscall>>
+    map<int, int> iscalling;  // map of <tid, iscalling>
 
     child = fork();
-    if(child==0){
+    if (child == 0) {
         int childLogFd = open("childLog", O_CREAT | O_RDWR | O_APPEND, 0x666);
         if (childLogFd < 0){
             printf("fail to open childlog\n");
@@ -48,28 +38,44 @@ int main(int argc,char **argv){
         ptrace(PTRACE_TRACEME,0,NULL,NULL);
         execvp(argv[1], NULL);
     } else {
-        while(1){
-            wait(&status);
+        while(1) {
+            int tid_return = wait(&status);
             if(WIFEXITED(status))
                 break;
-            orig_rax=ptrace(PTRACE_PEEKUSER,child,8*ORIG_RAX,NULL);
+            orig_rax = ptrace(PTRACE_PEEKUSER, tid_return, 8*ORIG_RAX, NULL);
 
-            if(orig_rax == SYS_open){
-                ptrace(PTRACE_GETREGS,child,NULL,&regs);
-                if(!iscalling){
-                    iscalling =1;
-                    char s[100];
-                    if (getFileName(child, regs.rdi, s, 100) < 0){
-                        printf("error\n");
-                        continue;
-                    };
-                    printf("SYS_open call with %lld, %lld, %lld, %s\n",regs.rdi,regs.rsi,regs.rdx, s);
-                } else{
-                    printf("SYS_open call return %lld\n",regs.rax);
-                    iscalling = 0;
+            // print
+            if (syscall[tid_return].find(orig_rax) == syscall[tid_return].end()) {
+                syscall[tid_return].insert(orig_rax);
+                printf("\nSYSCALL\n");
+                for (auto it = syscall.begin(); it != syscall.end(); it++) {
+                    printf("  # %d: ", it->first);
+                    for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+                        printf("\t%d", *it2);
+                    }
+                    printf("\n");
+                }
+            }
+            
+            // detect syscall: clone
+            if (orig_rax == SYS_clone) {
+                ptrace(PTRACE_GETREGS, tid_return, NULL, &regs);
+                if (iscalling[tid_return] != 1) {
+                    iscalling[tid_return] = 1;
+                    printf("SYS_clone call start in tid %d\n", tid_return);
+                } else {
+                    iscalling[tid_return] = 0;
+                    if (regs.rax != 0) {
+                        // return in parent thread (return value in child thread is 0)
+                        ptrace(PTRACE_ATTACH, regs.rax, NULL, NULL);
+                        iscalling[regs.rax] = 1;
+                    }
+                    printf("SYS_clone call return %lld in tid %d\n", regs.rax, tid_return);
                 }                                  
             }
-            ptrace(PTRACE_SYSCALL,child,NULL,NULL);
+
+            // wake up child parent
+            ptrace(PTRACE_SYSCALL, tid_return, NULL, NULL);
         }
     }
     return 0;
