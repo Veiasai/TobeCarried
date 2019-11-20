328ad7a3daa1a7ff30493aca82c8a05ccc491f02
gusabary <gusabary@126.com>
2019-11-20 12:39:26 +0000

    test(tracee): add init of mock rule manager and report

--- src/ptrace/tests/mock.h
+++ src/ptrace/tests/mock.h
@@ -12,6 +12,7 @@ namespace SAIL { namespace Test {
 using namespace testing;
 using namespace utils;
 using namespace core;
+using namespace rule;
 
 class MockCustomPtrace : public CustomPtrace
 {
@@ -27,6 +28,22 @@ class MockUtils : public Utils
 public:
     MOCK_METHOD(int, readStrFrom, (int tid, const char * p, char * buf, size_t s), (override));
     MOCK_METHOD(int, readBytesFrom, (int tid, const char * p, char * buf, size_t s), (override));
+    MOCK_METHOD(int, getFilenameByFd, (int tid, int fd, std::string &filename), (override));
 };
 
-}}
+class MockRuleManager : public RuleManager
+{
+public:
+    MOCK_METHOD(std::vector<core::RuleCheckMsg>, check, (int syscallNumber, const core::SyscallParameter &sp), (override));
+};
+
+class MockReport : public Report
+{
+public:
+    MOCK_METHOD(int, write, (const long tid, const long callID, const core::RuleCheckMsg &rcmsg), (override));
+    MOCK_METHOD(int, flush, (), (override));
+    MOCK_METHOD(size_t, size, (), (override));
+};
+
+} // namespace Test
+} // namespace SAIL
